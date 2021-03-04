c0b3e8fac537dc9caca5d57114201b5f8176bb56
retr00exepq <a838221222@sina.com>
2021-03-04 14:42:19 +0800

    Config file

--- js/server.js
+++ js/server.js
@@ -1,13 +1,69 @@
 const express = require("express");
 const app = express();
-const port = 3000;
 
+//middleware
 const createError = require("http-errors");
+const helmet = require("helmet");
 
-//
+///////////////////////////////////////////////////
+const debug = require("debug")("Break-It:server");
+const http = require("http");
+
+//get port from environment and store in Express
+const port = normalizePort(process.env.PORT || "3000");
+app.set("port", port);
+
+//create HTTP server
+const server = http.createServer(app);
+
+//listen on provided port on all network interfaces
+server.listen(port);
+server.on("error", onError);
+server.on("listening", onListening);
+
+//normalize port into a number, string, or false
+function normalizePort(val) {
+  let port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    //named pipe
+    return val;
+  } else if (port >= 0) {
+    //port number
+    return port;
+  } else {
+    return false;
+  }
+}
+
+//listen for HTTP server error
+function onError(error) {
+  if (error.syscall !== "listen") {
+    throw error;
+  }
+}
+
+///
+//load file
+const fs = require("fs");
+let stream;
+stream = fs.createReadStream("./config/config.json");
+stream.on("data", function (data) {
+  let chunk = data.toString();
+  console.log(chunk);
+});
+
+///
+
+///////////////////////////////////////////////////
+//secure HTTP headers
+app.use(helmet());
+
+// || err handling
 app.use(function (req, res, next) {
-  if (!req) {
-    return next(createError(404, "Page not found"));
+  if (req) {
+    // console.log(req);
+    // return next(createError(404, "Page not found"));
     next();
   }
 });
@@ -28,6 +84,7 @@ app.use(function (req, res, next) {
 //   }
 // });
 //
+///////////////////////////////////////////////////
 
 app.get("/", (req, res) => {
   res.send("Hello world!");
