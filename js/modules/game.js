47f1b9c59fe44b8c00a86fdaee8dda05ec4a2c5e
retr00exepq <a838221222@sina.com>
2021-03-20 21:01:11 +0800

    Game wrapper module

---
+++ js/modules/game.js
@@ -0,0 +1,35 @@
+class Game {
+  constructor() {
+    this.state = 0;
+    this.score = 0;
+    this.lives = 3;
+    this.won = false;
+  }
+  start() {
+    //only start game if no game is currently running
+    if (this.state === 0) {
+      //hide start/ win/ lose screens before starting game
+      if (!startDisplay.classList.contains("hidden")) {
+        startDisplay.classList.add("hidden");
+      } else if (!winDisplay.classList.contains("hidden")) {
+        winDisplay.classList.add("hidden");
+      } else if (!loseDisplay.classList.contains("hidden")) {
+        loseDisplay.classList.add("hidden");
+      }
+
+      //display canvas, lives and score
+      canvas.classList.remove("hidden");
+      slDisplay.classList.remove("hidden");
+      startSound.play();
+      draw();
+      this.state = 1; //game running
+    }
+  }
+  stop() {
+    //mark game as stopped and reset score/ lives
+    this.state = 0;
+    this.score = 0;
+    this.lives = 3;
+  }
+}
+let myGame = new Game();
