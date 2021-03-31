4da94146ca7c1bff9cb3d41751e59fdcdf5e0df7
retr00exepq <a838221222@sina.com>
2021-03-31 19:37:29 +0800

    Draw module

--- js/main.js
+++ js/main.js
@@ -131,72 +131,7 @@ function muteUmute() {
 }
 //////////////////////////////////////////
 
-//master function
-function draw() {
-  ctx.clearRect(0, 0, canvas.width, canvas.width);
-  myBricks.drawBricks();
-  drawBall();
-  drawPaddle();
-  drawScore();
-  drawLives();
-  collisionDetection();
-
-  //ball bounces off side wall
-  if (x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
-    wallSound.play();
-    dx = -dx;
-  }
-  //ball bounces off top wall
-  if (y + dy < 1) {
-    wallSound.play();
-    dy = -dy;
-  } else if (y + dy > canvas.height - ballRadius) {
-    if (x > paddleX && x < paddleX + paddleWidth) {
-      //paddle collision
-      paddleSound.play();
-      dy = -dy;
-    } else {
-      //ball hit ground
-      dropSound.play();
-      myGame.lives--;
-      if (!myGame.lives) {
-        //display lose screen, exit game and hide canvas/ score/ lives
-        loseSound.play();
-        myBricks.setUp();
-        displayScreen("loseDisplay");
-        canvas.classList.add("hidden");
-        slDisplay.classList.add("hidden");
-        myGame.stop();
-        myGame.lives = 4;
-        return;
-      } else {
-        x = canvas.width / 2;
-        y = canvas.height - 30;
-        dx = 2;
-        dy = -2;
-        paddleX = (canvas.width - paddleWidth) / 2;
-      }
-    }
-  }
-
-  //move paddle back and forth
-  if (rightPressed && paddleX < canvas.width - paddleWidth) {
-    paddleX += 5;
-  } else if (leftPressed && paddleX > 0) {
-    paddleX -= 5;
-  }
-
-  x += dx;
-  y += dy;
-
-  //exit when game won
-  if (myGame.won === true) {
-    myGame.won = false;
-    return;
-  }
-
-  requestAnimationFrame(draw);
-}
+///draw
 
 (function () {
   let message = "IIFE";
